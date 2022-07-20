3dc64d2d2ebb6dc9746a95b7674ce954e443c114
Liudmila S <limakova@gmail.com>
2022-07-20 14:34:47 +0300

    lesson changes

--- ui-testing/tests/workshop.spec.js
+++ ui-testing/tests/workshop.spec.js
@@ -1,4 +1,6 @@
 const { test, expect } = require('@playwright/test');
+const { SearchPage } = require('../pages/SearchPage');
+const { SearchResultsPage } = require('../pages/SearchResultsPage');
 
 test.describe('Search Results', () => {
    /* test('First result should contain devbrige.com', async({ page }) => {
@@ -9,17 +11,47 @@ test.describe('Search Results', () => {
         let hrefAttribute = await page.locator('#r1-0 h2 a').getAttribute('href');
 
         await expect(hrefAttribute).toContain('devbridge.com');
-        //await page.pause();
     });
-    */
+    
     test('Third result should contain linkedin.com', async({ page }) => {
         await page.goto('https://duckduckgo.com/');
         await page.locator('input[name=q]').fill('devbridge');
         await page.keyboard.press('Enter');
 
-        let hrefAttribute = await page.locator('#r1-2 h2 a').getAttribute('href');
+        let index = 2;
+        let hrefAttribute = await page.locator('#r1-${index} h2 a').getAttribute('href');
 
         await expect(hrefAttribute).toContain('linkedin.com');
         await page.pause();
     });
+    */
+
+    test('First result should contain devbridge.com', async ({ page }) => {
+        let searchPage = new SearchPage(page);
+        await searchPage.navigate();
+        await searchPage.search('devbridge');
+
+        let searchResultsPage = new SearchResultsPage(page);
+        expect(await searchResultsPage.getQueryInputText()).toEqual('devbridge');
+        expect(await searchResultsPage.getResultHeadingHrefAttribute(0)).toContain('devbridge.com');
+    });
+
+    test('Third result should contain linkedin.com', async ({ page }) => {
+        let searchPage = new SearchPage(page);
+        await searchPage.navigate();
+        await searchPage.search('devbridge');
+
+        let searchResultsPage = new SearchResultsPage(page);
+        expect(await searchResultsPage.getResultHeadingHrefAttribute(2)).toContain('linkedin.com');
+    });
+/*
+    test('Search query should get populated in Search Result page', async ({ page }) => {
+        let searchPage = new SearchPage(page);
+        await searchPage.navigate();
+        await searchPage.search('devbridge');
+
+        let searchResultsPage = new SearchResultsPage(page);
+        expect(await searchResultsPage.getQueryInputText()).toEqual('devbridge');
+    });
+*/
 });
